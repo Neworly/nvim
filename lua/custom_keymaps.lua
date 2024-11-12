f312c50e9734c4afd5b8ade05598196a4d663678
Neworly <piocarlo393@gmail.com>
2024-11-12 16:42:04 +0100

    telescope keys

--- lua/custom_keymaps.lua
+++ lua/custom_keymaps.lua
@@ -1,10 +1,7 @@
-
-
-
-
 local localmaps={}
 
 function setkey(m,seq,action,description)
+
 	description=description or {}
 	if(localmaps[seq] and localmaps[seq]==m)then
 		print("Attempt to overwrite an already set keymap for sequence: ",seq," and mode: ",m)
@@ -14,6 +11,11 @@ function setkey(m,seq,action,description)
 	vim.keymap.set(m,seq,action,description)
 end
 
+local tbuiltin=require("telescope.builtin")
+
+setkey('n',"<leader>ff",tbuiltin.find_files)
+setkey('n',"<leader>fg",tbuiltin.live_grep)
+setkey('n',"<leader>fb",tbuiltin.buffers)
 
 setkey('n', "<leader>cd", function() 
 	vim.cmd("colorscheme default")
@@ -22,7 +24,6 @@ end)
 setkey('n', "<leader>ca", function() 
 	vim.cmd("colorscheme afterglow")
 end)
-
 
 setkey('n', "<leader>co", function() 
 	vim.cmd("colorscheme oldworld")
