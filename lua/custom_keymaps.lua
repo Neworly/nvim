c8e114ede1bd02ebb69640f1fd206d8addb10478
Neworly <piocarlo393@gmail.com>
2024-11-12 15:23:20 +0000

    Colorscheme shortcuts, from default, afterglow, oldworld. I think they're gonna be our main themes

--- lua/custom_keymaps.lua
+++ lua/custom_keymaps.lua
@@ -1,9 +1,37 @@
 
 
 
-vim.keymap.set('n', "<leader>ca", function() 
+
+local localmaps={}
+
+function setkey(m,seq,action,description)
+	description=description or {}
+	if(localmaps[seq] and localmaps[seq]==m)then
+		print("Attempt to overwrite an already set keymap for sequence: ",seq,"and mode, ",m)
+		return
+	end
+	localmaps[seq]=m
+	vim.keymap.set(m,seq,action,description)
+end
+
+
+setkey('n', "<leader>cd", function() 
+	vim.cmd("colorscheme default")
+end)
+
+setkey('n', "<leader>cd", function() 
+	vim.cmd("colorscheme default")
+end)
+
+
+
+
+setkey('n', "<leader>ca", function() 
 	vim.cmd("colorscheme afterglow")
-end,{})
+end)
 
 
+setkey('n', "<leader>co", function() 
+	vim.cmd("colorscheme oldworld")
+end)
 
