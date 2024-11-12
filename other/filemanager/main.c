cd5eda276c7b965714d3916d2c4d8403ecdf8337
Neworly <piocarlo393@gmail.com>
2024-11-12 16:49:43 +0100

    TODO

--- other/filemanager/main.c
+++ other/filemanager/main.c
@@ -1,28 +1,15 @@
-
-
-
-
-
 #include<stdio.h>
-
-
 struct Folder;
 
 struct Folder{
-	struct Folder*file;
-	size_t size;
-	size_t amount;
-}
-
-#define DEF_FV() {NULL,0,0}
+  struct Folder*file;
+  size_t size;
+  size_t amount;
+};
 
 
-
-
-
-
+#define DEF_FV {NULL,0,0}
 int main(void){
-	struct Folder root=DEF_FV();
 
-	return 0;
+  return 0;
 }
